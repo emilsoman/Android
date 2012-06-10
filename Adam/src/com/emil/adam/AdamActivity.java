205500d2185132e35c7cacee7f5b541fa9088311
Emil Soman <emil.soman@gmail.com>
2012-06-11 04:51:03 +0530

    Project Adam added to repository

---
+++ Adam/src/com/emil/adam/AdamActivity.java
@@ -0,0 +1,13 @@
+package com.emil.adam;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class AdamActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
