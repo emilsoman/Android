9d6af23497ce3ab13a6097b31e346f41a386166d
Emil Soman <emil.soman@gmail.com>
2012-06-14 22:10:17 +0530

    buttons added in main page

--- Adam/assets/www/mapapp.js
+++ Adam/assets/www/mapapp.js
@@ -1,2 +1,27 @@
 var mapdata = { destination: new google.maps.LatLng(59.3327881, 18.064488100000062) };
 
+//$(window).resize(function(){
+//
+//    $('.vertical-middle').css({
+//        position:'absolute',
+//        left: ($(window).width() - $('.vertical-middle').outerWidth())/2,
+//        top: ($(window).height() - $('.vertical-middle').outerHeight())/2
+//    });
+//
+//});
+
+//$('#page-home').live("pageinit", function() {
+//    //$(window).resize();
+//    $('.vertical-middle').css({
+//        position:'absolute',
+//        left: ($(window).width() - $('.vertical-middle').outerWidth())/2,
+//        top: ($(window).height() - $('.vertical-middle').outerHeight())/2
+//    });
+//});
+
+
+
+
+
+
+
